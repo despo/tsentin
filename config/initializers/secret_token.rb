9260865e57b4003bca3c295799e7fa44b481586c
Despo Pentara <despo@extractmethod.com>
2013-03-29 23:14:04 +0000

    let's make a place where anyone with a facebook account can post offers they see/find

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TsentIn::Application.config.secret_token = '1c4a4e55239ca55965d6f10b146b700c604661151c920184ae3dab22ab0d3a35460d386f8c522bd1a77b2f3b58615c4ecbff163fdc27f154a95d1b5e4fcbdbc7'
