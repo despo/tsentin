191280a942c926e21a692b597fe9726ea9361a53
Despo Pentara <despo@extractmethod.com>
2013-03-31 20:09:15 +0100

    add cities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331172113) do
+ActiveRecord::Schema.define(:version => 20130331190156) do
+
+  create_table "cities", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
