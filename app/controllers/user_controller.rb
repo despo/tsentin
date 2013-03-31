ee083c996c69d1cd6e5ed52682efc27c1db749ae
Despo Pentara <despo@extractmethod.com>
2013-03-31 18:39:00 +0100

    fix query lol

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,6 +2,6 @@ class UserController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @posts = Post.limit(5).find_by_user_id(current_user.id)
+    @posts = Post.limit(5).where(user_id: current_user.id)
   end
 end
